4091befb19d233c695d9426b4344c9568eff3b00
VitaliyDmitrienko <vddjack2008@yandex.ru>
2023-08-25 23:57:08 +0300

    1st. + 28.2.1.

--- src/main/java/org/example/Main2821.java
+++ src/main/java/org/example/Main2821.java
@@ -3,39 +3,48 @@ package org.example;
 import java.lang.reflect.Modifier;
 
 public class Main2821 {
-
     public static void main(String[] args) throws ClassNotFoundException {
-        // Получаем класс для java.util.HashMap
-        Class<?> hashMap = Class.forName("java.util.HashMap");
 
-        // Получаем класс для java.util.AbstractMap
-        Class<?> abstractMap = Class.forName("java.util.AbstractMap");
+        Class<?> arrayList = Class.forName("java.util.ArrayList");
+        Class<?> abstractList = Class.forName("java.util.AbstractList");
+        Class<?> collection = Class.forName("java.util.Collection");
 
-        // Получаем класс для java.util.Map
-        Class<?> map = Class.forName("java.util.Map");
-
-
-        //Получаем модификаторы для HashMap
-        int hashMapModifiers = hashMap.getModifiers();
-
-        //Получаем модификаторы для AbstractMap
-        int abstractMapModifiers = abstractMap.getModifiers();
-
-        //Получаем модификаторы для Map
-        int mapModifiers = map.getModifiers();
+        int arrayListModifiers = arrayList.getModifiers();
+        int abstractListModifiers = abstractList.getModifiers();
+        int collectionModifiers = collection.getModifiers();
 
 
-        //Выводим, имеет ли HashMap public модификатор
-        System.out.println("HashMap class is public :" + Modifier.isPublic(hashMap.getModifiers()));
-//        System.out.println("HashMap class is public :" + Modifier.isPublic(hashMapModifiers));
+        System.out.println("ArrayList class is public? \"" + Modifier.isPublic(arrayList.getModifiers())+ "\".");
+        System.out.println("ArrayList class is private? \"" + Modifier.isPrivate(arrayList.getModifiers())+ "\".");
+        System.out.println("ArrayList class is protected? \"" + Modifier.isProtected(arrayList.getModifiers())+ "\".");
+        System.out.println("ArrayList class is abstract? \"" + Modifier.isAbstract(arrayList.getModifiers())+ "\".");
+        System.out.println("ArrayList class is final? \"" + Modifier.isFinal(arrayList.getModifiers())+ "\".");
+        System.out.println("ArrayList class is static? \"" + Modifier.isStatic(arrayList.getModifiers())+ "\".");
+        System.out.println("ArrayList class is strict? \"" + Modifier.isStrict(arrayList.getModifiers())+ "\".");
+        System.out.println("ArrayList class is synchronized? \"" + Modifier.isSynchronized(arrayList.getModifiers())+ "\".");
+        System.out.println("ArrayList class is Interface? \"" + Modifier.isInterface(arrayList.getModifiers())+ "\".");
+        System.out.println();
 
-        //Выводим, имеет ли AbstractMap abstract модификатор
-        System.out.println("AbstractMap class is abstract :" + Modifier.isAbstract(abstractMapModifiers));
+        System.out.println("AbstractList class is public? \"" + Modifier.isPublic(abstractList.getModifiers())+ "\".");
+        System.out.println("AbstractList class is private? \"" + Modifier.isPrivate(abstractList.getModifiers())+ "\".");
+        System.out.println("AbstractList class is protected? \"" + Modifier.isProtected(abstractList.getModifiers())+ "\".");
+        System.out.println("AbstractList class is abstract? \"" + Modifier.isAbstract(abstractList.getModifiers())+ "\".");
+        System.out.println("AbstractList class is final? \"" + Modifier.isFinal(abstractList.getModifiers())+ "\".");
+        System.out.println("AbstractList class is static? \"" + Modifier.isStatic(abstractList.getModifiers())+ "\".");
+        System.out.println("AbstractList class is strict? \"" + Modifier.isStrict(abstractList.getModifiers())+ "\".");
+        System.out.println("AbstractList class is synchronized? \"" + Modifier.isSynchronized(abstractList.getModifiers())+ "\".");
+        System.out.println("AbstractList class is Interface? \"" + Modifier.isInterface(abstractList.getModifiers())+ "\".");
+        System.out.println();
 
-        //Выводим, имеет ли Map interface модификатор
-        System.out.println("Map is interface :" + Modifier.isInterface(mapModifiers));
+        System.out.println("Collection class is public? \"" + Modifier.isPublic(collectionModifiers)+ "\".");
+        System.out.println("Collection class is private? \"" + Modifier.isPrivate(collectionModifiers)+ "\".");
+        System.out.println("Collection class is protected? \"" + Modifier.isProtected(collectionModifiers)+ "\".");
+        System.out.println("Collection class is abstract? \"" + Modifier.isAbstract(collectionModifiers)+ "\".");
+        System.out.println("Collection class is final? \"" + Modifier.isFinal(collectionModifiers)+ "\".");
+        System.out.println("Collection class is static? \"" + Modifier.isStatic(collectionModifiers)+ "\".");
+        System.out.println("Collection class is strict? \"" + Modifier.isStrict(collectionModifiers)+ "\".");
+        System.out.println("Collection class is synchronized? \"" + Modifier.isSynchronized(collectionModifiers)+ "\".");
+        System.out.println("Collection class is Interface? \"" + Modifier.isInterface(collectionModifiers)+ "\".");
 
-        //Выводим, имеет ли HashMap abstract модификатор
-        System.out.println("HashMap class is abstract :" + Modifier.isAbstract(hashMapModifiers));
     }
 }
